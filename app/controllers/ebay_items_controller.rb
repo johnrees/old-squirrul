23b3642c95ac3fa0f423efc837166f86896d6b97
John Rees <john@bitsushi.com>
2016-03-06 20:38:36 +0000

    sort ebay items in desc order

--- app/controllers/ebay_items_controller.rb
+++ app/controllers/ebay_items_controller.rb
@@ -3,7 +3,7 @@ class EbayItemsController < ApplicationController
   before_action :authorize
 
   def index
-    @ebay_items = EbayItem.all
+    @ebay_items = EbayItem.order(ends_at: :asc)
     @ebay_bids = EbayBid.where(user: current_user)
   end
 
