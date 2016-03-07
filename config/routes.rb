b5a8ac4c37831cfa83a3ab120fcd4a28e44708f4
John Rees <john@bitsushi.com>
2016-03-07 10:24:43 +0000

    html header, removed sign up link

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-
-  resources :users
-  resources :sessions
-  resources :ebay_items
-  resources :snipes
-
-  root to: 'static#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+
+  # get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  # resources :users
+  resources :sessions
+  resources :ebay_items
+  resources :snipes
+
+  root to: 'snipes#index'
 end
