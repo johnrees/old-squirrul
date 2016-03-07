93b0ad18a334bcf5332d3849537763837d329df3
John Rees <john@bitsushi.com>
2016-03-07 12:54:28 +0000

    use nil for watching snipes

--- app/models/snipe.rb
+++ app/models/snipe.rb
@@ -4,7 +4,7 @@ class Snipe < ApplicationRecord
   belongs_to :ebay_item
 
   register_currency :gbp
-  monetize :max_amount_cents
+  monetize :max_amount_cents, allow_nil: true
 
   validates_presence_of :user, :ebay_item
   validates_uniqueness_of :user, scope: :ebay_item
@@ -15,7 +15,7 @@ class Snipe < ApplicationRecord
   end
 
   def state
-    if max_amount_cents.blank? or max_amount_cents == 0
+    if max_amount_cents.blank?
       "watching"
     elsif max_amount >= ebay_item.min_bid_price
       "valid"
