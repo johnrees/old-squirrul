e75004f3f55234f993489ffe7b96307dce7c7770
John Rees <john@bitsushi.com>
2016-03-09 12:25:10 +0000

    puma threads

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,35 +1,40 @@
-require 'rufus-scheduler'
+if $scheduler_thread
 
-times = [
-  [0, 2.seconds],
-  [15.seconds, 5.seconds],
-  [1.minute, 30.seconds],
-  [5.minutes, 1.minute],
-  [15.minutes, 5.minutes],
-  [1.hour, 20.minutes]
-].reverse!
+  require 'rufus-scheduler'
 
-ebay_items = EbayItem.upcoming.pluck("id,ends_at")
+  Rails.logger.info "Created rufus scheduler"
+  scheduler = Rufus::Scheduler.new
 
-Rails.logger.info $$
+  times = [
+    [0, 2.seconds],
+    [15.seconds, 5.seconds],
+    [1.minute, 30.seconds],
+    [5.minutes, 1.minute],
+    [15.minutes, 5.minutes],
+    [1.hour, 20.minutes]
+  ].reverse!
 
-Rufus::Scheduler.singleton.every '2s' do #, lockfile: '.scheduler-lockfile'
-  now = Time.now.to_i
-  Rails.logger.info now
-  ebay_items.each do |id,ends_at|
-    time_left = (ends_at - now).to_i
-    times.each do |limit, frequency|
-      if time_left >= limit
-        if time_left % frequency <= 1
-          str = "#{id} - #{time_left.to_s}"
-          str += " #{Time.at(frequency).utc.strftime("%H:%M:%S")}"
-          Rails.logger.info str
-          EbayItem.find(id).scrape!
+  ebay_items = EbayItem.upcoming.pluck("id,ends_at")
+
+  scheduler.every '2s' do #, lockfile: '.scheduler-lockfile'
+    now = Time.now.to_i
+    Rails.logger.info now
+    ebay_items.each do |id,ends_at|
+      time_left = (ends_at - now).to_i
+      times.each do |limit, frequency|
+        if time_left >= limit
+          if time_left % frequency <= 1
+            str = "#{id} - #{time_left.to_s}"
+            str += " #{Time.at(frequency).utc.strftime("%H:%M:%S")}"
+            Rails.logger.info str
+            EbayItem.find(id).scrape!
+          end
+          break
         end
-        break
       end
     end
   end
-end
 
-# scheduler.join
+  # scheduler.join
+
+end
