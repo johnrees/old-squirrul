f8fef99a6576a31f31c006f46d057e5cb98d4a3f
John Rees <john@bitsushi.com>
2016-03-13 15:09:28 +0000

    spec updates

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe User, :type => :model do
     expect(build_stubbed(:user)).to be_valid
   end
 
+  it "has secure_password" do
+    expect(User).to respond_to(:has_secure_password)
+  end
+
   it "validates uniqueness of username" do
     user # initialize :let object
     expect{
@@ -15,10 +19,17 @@ RSpec.describe User, :type => :model do
     }.to raise_error(/Username has already been taken/)
   end
 
-  skip "can prepare a snipe" do
-    ebay_item = create(:ebay_item)
-    # expect(user.snipes).to receive(:create).with("an argument I want")
-    user.make_snipe(ebay_item, 100)
+  it "has many snipes" do
+    snipe = create(:snipe, user: user)
+    expect(user.snipes).to eq([snipe])
   end
+
+  it "can authenticate with ebay"
+
+  it "can prepare a snipe"
+  #   ebay_item = create(:ebay_item)
+  #   # expect(user.snipes).to receive(:create).with("an argument I want")
+  #   user.make_snipe(ebay_item, 100)
+  # end
 
 end
